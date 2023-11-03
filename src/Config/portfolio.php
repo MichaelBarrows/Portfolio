f17cb6a99fb97b0281401f5cc225973f515c3e88
Michael Barrows <m+github@mbws.uk>
2023-11-03 14:02:00 +0000

    config tweaks

--- src/Config/portfolio.php
+++ src/Config/portfolio.php
@@ -2,4 +2,14 @@
 
 return [
     'portfolio-api-domain' => env('PORTFOLIO_API_DOMAIN'),
+    'database' => [
+        'url' => env('PORTFOLIO_DATABASE_URL'),
+        'host' => env('PORTFOLIO_DB_HOST', '127.0.0.1'),
+        'port' => env('PORTFOLIO_DB_PORT', '3306'),
+        'database' => env('PORTFOLIO_DB_DATABASE', 'forge'),
+        'username' => env('PORTFOLIO_DB_USERNAME', 'forge'),
+        'password' => env('PORTFOLIO_DB_PASSWORD', ''),
+        'unix_socket' => env('PORTFOLIO_DB_SOCKET', ''),
+        'MYSQL_ATTR_SSL_CA' => env('MYSQL_ATTR_SSL_CA'),
+    ],
 ];
--- src/Providers/PortfolioServiceProvider.php
+++ src/Providers/PortfolioServiceProvider.php
@@ -25,15 +25,19 @@ class PortfolioServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__.'/../Routes/portfolio.php');
         $this->loadMigrationsFrom(__DIR__.'/../Database/Migrations');
 
+        $this->publishes([
+            __DIR__.'/../Config/portfolio.php' => config_path('portfolio.php'),
+        ], 'portfolio');
+
         config(['database.connections.portfolio' => [
                 'driver' => 'mysql',
-                'url' => env('PORTFOLIO_DATABASE_URL'),
-                'host' => env('PORTFOLIO_DB_HOST', '127.0.0.1'),
-                'port' => env('PORTFOLIO_DB_PORT', '3306'),
-                'database' => env('PORTFOLIO_DB_DATABASE', 'forge'),
-                'username' => env('PORTFOLIO_DB_USERNAME', 'forge'),
-                'password' => env('PORTFOLIO_DB_PASSWORD', ''),
-                'unix_socket' => env('PORTFOLIO_DB_SOCKET', ''),
+                'url' => config('portfolio.database.url', env('PORTFOLIO_DATABASE_URL')),
+                'host' => config('portfolio.database.host', env('PORTFOLIO_DB_HOST', '127.0.0.1')),
+                'port' => config('portfolio.database.port', env('PORTFOLIO_DB_PORT', '3306')),
+                'database' => config('portfolio.database.database', env('PORTFOLIO_DB_DATABASE', 'forge')),
+                'username' => config('portfolio.database.username', env('PORTFOLIO_DB_USERNAME', 'forge')),
+                'password' => config('portfolio.database.password', env('PORTFOLIO_DB_PASSWORD', '')),
+                'unix_socket' => config('portfolio.database.socket', env('PORTFOLIO_DB_SOCKET', '')),
                 'charset' => 'utf8mb4',
                 'collation' => 'utf8mb4_unicode_ci',
                 'prefix' => '',
@@ -41,12 +45,8 @@ class PortfolioServiceProvider extends ServiceProvider
                 'strict' => true,
                 'engine' => null,
                 'options' => extension_loaded('pdo_mysql') ? array_filter([
-                    PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                    PDO::MYSQL_ATTR_SSL_CA => config('portfolio.database.MYSQL_ATTR_SSL_CA', env('MYSQL_ATTR_SSL_CA')),
                 ]) : [],
         ]]);
-
-        $this->publishes([
-            __DIR__.'/../Config/portfolio.php' => config_path('portfolio.php'),
-        ], 'portfolio');
     }
 }
