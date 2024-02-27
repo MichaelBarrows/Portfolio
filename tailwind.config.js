bb00fbebefb53f42e58a72663e94126af6f932c1
Michael Barrows <m+github@mbws.uk>
2024-02-27 19:30:15 +0000

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {
+        colors: {
+            'pacific-blue': {
+                '50': '#f0faff',
+                '100': '#e0f5fe',
+                '200': '#b9ecfe',
+                '300': '#7cdffd',
+                '400': '#36d0fa',
+                '500': '#0cbaeb',
+                '600': '#0099cc',
+                '700': '#0178a3',
+                '800': '#066586',
+                '900': '#0b536f',
+                '950': '#07354a',
+            },
+            'off-white': '#f9fafb'
+        }
+    },
+  },
+  plugins: [],
+}
+
