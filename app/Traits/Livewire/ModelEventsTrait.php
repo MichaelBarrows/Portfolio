9b921f7fc3478d4ba569a5652c9051e3ddcba87f
Michael Barrows <m+github@mbws.uk>
2024-08-15 19:23:48 +0100

    Added trait for handling model events in livewire

---
+++ app/Traits/Livewire/ModelEventsTrait.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Traits\Livewire;
+
+trait ModelEventsTrait
+{
+    public array $refreshModelFields;
+
+    public function modelCreated()
+    {
+        $this->getData();
+    }
+
+    public function modelUpdated(string $key, array $event)
+    {
+        if ($this->data->has($key)) {
+            foreach ($this->refreshModelFields as $field) {
+                if (array_key_exists($field, $event)) {
+                    return $this->data[$key]->refresh();
+                }
+            }
+
+            return $this->data[$key]->fill($event);
+        }
+
+        return $this->getData();
+    }
+
+    public function modelDeleted(string $key, array $event)
+    {
+        $this->data->forget($key);
+    }
+}
---
+++ tests/Unit/Traits/Livewire/ModelEventsTraitTest.php
@@ -0,0 +1,67 @@
+<?php
+
+use App\Models\Education;
+use App\Traits\Livewire\ModelEventsTrait;
+use Illuminate\Support\Collection;
+
+beforeEach(function () {
+    $this->class = new class () {
+        use ModelEventsTrait;
+
+        public Collection $data;
+
+        public function __construct()
+        {
+            $this->refreshModelFields = ['description'];
+            $this->getData();
+        }
+
+        public function getData()
+        {
+            $this->data = Education::all()->keyBy('id');
+        }
+    };
+});
+
+it('modelCreated calls the getData method', function () {
+    $this->class->getData();
+    expect($this->class->data)->isEmpty()->toBeTrue();
+    Education::factory(2)->create();
+
+    $this->class->modelCreated([]);
+
+    expect($this->class->data)->count()->toBe(2);
+});
+
+it('modelUpdated applies the updates to the models', function () {
+    $education = Education::factory()->create();
+    $this->class->getData();
+    expect($this->class->data)->count()->toBe(1);
+
+    $this->class->modelUpdated($education['id'], ['course_name' => 'test 2']);
+
+    expect($this->class->data->first())->course_name->toBe('test 2');
+});
+
+it('modelUpdated reloads the whole model when the description key is provided', function () {
+    $education = Education::factory()->create();
+    $this->class->getData();
+    expect($this->class->data)->count()->toBe(1);
+    $education->update([
+        'course_name' => 'test course',
+    ]);
+
+    $this->class->modelUpdated($education['id'], ['description' => 'some description', 'course_name' => 'test 2']);
+
+    expect($this->class->data->first())->course_name->toBe('test course');
+});
+
+it('modelDeleted removes the model from the dataset', function () {
+    $education = Education::factory()->create();
+    $this->class->getData();
+    expect($this->class->data)->count()->toBe(1);
+
+    $this->class->modelDeleted($education->getKey(), []);
+
+    expect($this->class->data)->isEmpty()->toBeTrue();
+});
