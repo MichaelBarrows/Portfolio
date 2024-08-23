790be73392f7af00b6d004f274f63ce77b4c48d5
Michael Barrows <m+github@mbws.uk>
2024-08-23 19:11:10 +0100

    handle tech stack array -> collection

--- app/Traits/Event/GeneratesModelEventPayload.php
+++ app/Traits/Event/GeneratesModelEventPayload.php
@@ -14,6 +14,10 @@ trait GeneratesModelEventPayload
             $payload['type'] = $type;
         }
 
+        if (array_key_exists('tech_stack', $data) && is_array($data['tech_stack'])) {
+            $data['tech_stack'] = collect($data['tech_stack']);
+        }
+
         foreach ($filterableKeys as $filterableKey) {
             if (array_key_exists($filterableKey, $data)) {
                 $payload['filtered'] = true;
