c4491e5d1567885cf177fb713ee30801df4a96e3
Michael Barrows <m+github@mbws.uk>
2024-09-14 17:48:11 +0100

    styling & filament assets

--- resources/css/filament/admin/tailwind.config.js
+++ resources/css/filament/admin/tailwind.config.js
@@ -1,4 +1,5 @@
 import preset from '../../../../vendor/filament/filament/tailwind.config.preset'
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
     presets: [preset],
@@ -8,4 +9,24 @@ export default {
         './resources/views/livewire/*.blade.php',
         './vendor/filament/**/*.blade.php',
     ],
+    theme: {
+        extend: {
+            colors: {
+                'pacific-blue': {
+                    '50': '#f0faff',
+                    '100': '#e0f5fe',
+                    '200': '#b9ecfe',
+                    '300': '#7cdffd',
+                    '400': '#36d0fa',
+                    '500': '#0cbaeb',
+                    '600': '#0099cc',
+                    '700': '#0178a3',
+                    '800': '#066586',
+                    '900': '#0b536f',
+                    '950': '#07354a',
+                },
+            }
+        }
+    }
 }
+
