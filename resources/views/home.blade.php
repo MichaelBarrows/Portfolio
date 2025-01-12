cf60ca36984197b7b3f41eac18a834156525d778
Michael Barrows <m+github@mbws.uk>
2025-01-12 16:24:56 +0000

    add og:site_name property

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Michael Barrows | Software Engineer in Colchester, England</title>
+        <meta property="og:site_name" content="Michael Barrows" />
 
         @filamentStyles
         @vite('resources/css/app.css')
